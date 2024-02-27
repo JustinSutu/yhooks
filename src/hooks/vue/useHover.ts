257bf1a67d9994c27c6b946f1512e0bfe0a9c41f
myth929 <422896780@qq.com>
2024-02-27 20:49:59 +0800

    add vue-demo && add useHover for vue

---
+++ src/hooks/vue/useHover.ts
@@ -0,0 +1,49 @@
+import { onMounted, ref, Ref } from "vue"
+
+interface Options {
+    delayEnter?: number;
+    delayLeave?: number;
+}
+
+type UseHover = (options: Options) => {
+    isHover: Ref<boolean>;
+    elementRef: Ref<HTMLElement | null>;
+}
+
+const useHover: UseHover = (options = {}) => {
+    const isHover = ref<boolean>(false);
+    const elementRef = ref<HTMLElement | null>(null);
+
+    const {
+        delayEnter = 0,
+        delayLeave = 0
+    } = options
+
+    let timer: ReturnType<typeof setTimeout> | undefined
+
+    const toggle = (entering: boolean) => {
+        const delay = entering ? delayEnter : delayLeave
+        if (timer) {
+            clearTimeout(timer)
+            timer = undefined
+        }
+
+        if (delay) {
+            timer = setTimeout(() => isHover.value = entering, delay)
+        }
+        else {
+            isHover.value = entering
+        }
+    }
+    onMounted(() => {
+        if (!elementRef.value) {
+            return;
+        }
+        elementRef.value.addEventListener('mouseenter', () => toggle(true));
+        elementRef.value.addEventListener('mouseleave', () => toggle(false));
+    })
+
+    return {isHover, elementRef}
+}
+
+export default useHover;
