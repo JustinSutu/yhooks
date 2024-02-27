dcafbfbfa1c860b126c72cbaec245ed298162a0d
v_zhangshuangyi <v_zhangshuangyi@bilibili.com>
2024-02-27 10:38:58 +0800

    fix: 修复CodeReview代码风格问题

--- src/hooks/vue/useToggle.ts
+++ src/hooks/vue/useToggle.ts
@@ -1,12 +1,13 @@
-import {ref, Ref} from 'vue'
-const useToggle: (initialState?: boolean) => [Ref<boolean>, (state?: boolean) => void] = (initialState = false) => {
-
-  const state = ref(initialState)
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+export const useToggle: (initialState?: boolean) => [Ref<boolean>, (state?: boolean) => void] = (
+  initialState = false,
+) => {
+  const state = ref(initialState);
 
   const onToggle = (newState?: boolean) => {
-    state.value = newState ?? !state.value
-  }
+    state.value = newState ?? !state.value;
+  };
 
-  return [state, onToggle]
-}
-export default useToggle;
+  return [state, onToggle];
+};
